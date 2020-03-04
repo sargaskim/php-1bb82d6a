95058d2cce01dc084207da0c86a946311139a242
sargaskim <2103702@talnet.nl>
2020-03-04 11:44:15 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+
+    <header></header>
+
+    <main>
+
+    <?php
+    
+    echo 'bit academy';
+    
+    ?>
+
+
+    </main>
+    
+    <footer></footer>
+
+</body>
+
+</html>
